58004130f1554bb7f785066ca57453c932de80d9
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-24 20:41:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,18 @@
 class Book
-  def initialize(title)
-    @title = title
-  end
- 
-  def title
-  end
+
+    def initialize(title)
+      @title = title
+    end
+
+    def title
+      @title
+    end
+
+    def author=(author)
+      @author = author
+    end
+
+    def author
+      @author
+    end
 end
